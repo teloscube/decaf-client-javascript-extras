a6bffe106f2897d7a732772be6be1b26f5b3b97c
Ali Oguzhan Yildiz <aoguzhanyildiz@gmail.com>
2022-07-26 11:51:24 +0300

    chore: do not move compiled files to root

--- postinstall.sh
+++ postinstall.sh
@@ -3,10 +3,10 @@ set -e
 
 if [ -n "$DECAF_JS_SKIP_POSTINSTALL" ]; then
   yarn husky install
-  echo "Local Env detected. Skipping dist/ relocation..."
+  echo "Local Env detected. Skipping lib/ relocation..."
   exit 0
 fi
 
-echo 'Copying files from dist folder into root project folder...'
-cp -r dist/* ./ && rm -rf dist
+echo 'Copying files from lib folder into root project folder...'
+cp -r lib/* ./ && rm -rf lib
 echo 'Done!'
