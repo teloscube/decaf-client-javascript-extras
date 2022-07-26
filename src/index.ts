34ce139bacc1884bc54f6525ffdcdb4157670807
Ali Oguzhan Yildiz <aoguzhanyildiz@gmail.com>
2022-07-26 10:21:59 +0300

    chore: do not export everything from index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export * from './prelude';
-export * from './utils';
-export * from './valuation';
+// export * from './prelude';
+// export * from './utils';
+// export * from './valuation';
+export const bir = 1;
