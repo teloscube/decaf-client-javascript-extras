b4915aace037ce9e56be9161ea730529a7d31536
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2023-01-31 15:23:55 +0800

    refactor!: change type of CurrencyCode, use regexp for validation
    BREAKING CHANGE: The underlying type of CurrencyCode has been changed
    to a simpler, tagged type definition. Call-sites should use either of:
    
    ```
    x as string
    ```
    
    or
    
    ```
    unCurrencyCode(x)
    ```
    
    instead of `unPhantom`.
    
    Also, we are now using a regular expression (^[A-Z][a-zA-Z]*$) for
    rudimentary validation.

--- src/commons/-currency.ts
+++ src/commons/-currency.ts
@@ -4,27 +4,40 @@
  * @module
  */
 
-import { Just, Maybe, mkPhantom, NewTypeWithPhantom, Nothing, unPhantom } from '@telostat/prelude';
+import { Just, Maybe, Nothing } from '@telostat/prelude';
 
 /**
- * Type encoding for currency code values.
+ * Type definition for currency code values.
  *
- * A currency code is (typically) defined as all-uppercase, three letters.
+ * A currency code is (typically) defined as all-uppercase, three-letter
+ * `string`. In this library, we are particularly using the regular expression
+ * {@link REGEXP_CURRENCY_CODE} which allows any uppercase letter followed by
+ * uppercase or lowercase letters.
+ *
+ * As for the type definition: We are using a (not-so-elegant) trick called
+ * "tagged-type" to annotate the plain-vanilla `string` type to be the
+ * `CurrencyCode` (_poor man's newtype_). Note that during the runtime, all
+ * `CurrencyCode` values are simply a `string` values.
  */
-export type CurrencyCode = NewTypeWithPhantom<'CurrencyCode', string>;
+export type CurrencyCode = string & { readonly __tag: unique symbol };
+
+/**
+ * Regular expression for currency codes in our concept.
+ */
+export const REGEXP_CURRENCY_CODE = /^[A-Z][a-zA-Z]*$/;
 
 /**
  * Attempts to create a {@link CurrencyCode} value with the given currency code.
  *
- * This function returns a `Maybe` value. For the version that throws an
- * error, see {@link mkCurrencyCodeError}.
+ * This function returns a `Maybe` value. For the
+ * version that throws an error, see {@link mkCurrencyCodeError}.
  *
  * @param x A currency code represented as string.
  * @returns If the argument has leading or trailing spaces OR it is empty,
  * `Nothing` is retured, `Just` {@link CurrencyCode} otherwise.
  */
 export function mkCurrencyCode(x: string): Maybe<CurrencyCode> {
-  return x !== x.trim() || x === '' ? Nothing : Just(mkPhantom(x));
+  return REGEXP_CURRENCY_CODE.test(x) ? Just(x as CurrencyCode) : Nothing;
 }
 
 /**
@@ -40,16 +53,18 @@ export function mkCurrencyCode(x: string): Maybe<CurrencyCode> {
  */
 export function mkCurrencyCodeError(x: string): CurrencyCode {
   return mkCurrencyCode(x).orDefaultLazy(() => {
-    throw new Error(`Invalid currency code: "${x}"`);
+    throw new Error(`Invalid currency code: ${x}`);
   });
 }
 
 /**
  * Return the currency code as a string value.
  *
+ * In runtime, this is effectively the identity function.
+ *
  * @param x {@link CurrencyCode} to be extracted string from.
  * @returns Currency code as a string.
  */
 export function unCurrencyCode(x: CurrencyCode): string {
-  return unPhantom(x);
+  return x as string;
 }
