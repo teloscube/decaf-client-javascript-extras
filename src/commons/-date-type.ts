7801283bdc72463bbb243f03c5bb10ef77c5dd7a
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2023-01-31 15:28:00 +0800

    refactor!: rename constant dateTypes to DATE_TYPES
    BREAKING CHANGE: We are now adopting the convention that constants
    are defined via all-uppercase, snake-case symbols.

--- src/commons/-date-type.ts
+++ src/commons/-date-type.ts
@@ -6,7 +6,7 @@ export type DateType = 'commitment' | 'settlement';
 /**
  * Translation table for DECAF FINREA action date types.
  */
-export const dateTypes: { label: string; value: DateType }[] = [
+export const DATE_TYPES: { label: string; value: DateType }[] = [
   { label: 'Trade Date', value: 'commitment' },
   { label: 'Value Date', value: 'settlement' },
 ];
