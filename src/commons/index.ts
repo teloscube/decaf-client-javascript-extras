ee6b7a79f909d4e62ef0a5344aa2a7369852aa9d
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2023-01-31 20:25:27 +0800

    refactor: re-export common sub-modules

--- src/commons/index.ts
+++ src/commons/index.ts
@@ -1,3 +1,6 @@
 export * from './-currency';
+export * as currency from './-currency';
 export * from './-datetype';
+export * as datetype from './-datetype';
 export * from './-id';
+export * as id from './-id';
